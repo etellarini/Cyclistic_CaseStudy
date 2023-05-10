ec0e87327b18fd7c5f2ab8aa6e6ac3892d8b3efe
etellarini <128699080+etellarini@users.noreply.github.com>
2023-05-10 10:47:56 -0300

    StationClean.sql

--- StationClean.sql
+++ StationClean.sql
@@ -2,7 +2,3 @@
 UPDATE [Cyclistic].[dbo].[TripData_Integrated12m_StationsAdd]
 SET [ended_Location] = SUBSTRING([ended_Location], 2, LEN([ended_Location]) - 2)
 WHERE [ended_Location] LIKE '(%'
-
-
-
-
