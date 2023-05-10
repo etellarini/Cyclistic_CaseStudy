f26c915f584668bf86fe58b42b5e9118d570b9b7
etellarini <128699080+etellarini@users.noreply.github.com>
2023-05-10 17:08:46 -0300

    Quarter Tables Separation.sql

--- Quarter Tables Separation.sql
+++ Quarter Tables Separation.sql
@@ -1,6 +1,7 @@
+--Example of 2022_Q3 separation--
+
 SELECT *
 INTO [TripData_2022_Q3]
 FROM [Cyclistic].[dbo].[TripData_Integrated12m_StationsAdd]
 WHERE [started_date] >= '2022-07-01'
 AND [started_date] < '2022-10-01';
-
